785b2922b94a36a1b3d32baffa64b4ef1e1e8a15
MohamedMans0ur <132812009+MohamedMans0ur@users.noreply.github.com>
2023-09-21 09:56:17 +0300

    init

--- app/src/main/java/com/mobilemasr/platform/MainActivity.kt
+++ app/src/main/java/com/mobilemasr/platform/MainActivity.kt
@@ -10,18 +10,18 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.mobilemasr.platform.databinding.ActivityMainBinding
+//import com.mobilemasr.platform.databinding.ActivityMainBinding
 import com.mobilemasr.platform.ui.theme.MobileMasr_PlatformTheme
 
 class MainActivity : ComponentActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+//    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+//        binding = ActivityMainBinding.inflate(layoutInflater)
+//        setContentView(binding.root)
 
     }
 }
